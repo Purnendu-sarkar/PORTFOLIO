732021403651115cb793392420bdf6d7ce559871
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-08-31 00:49:18 +0600

    chore: initialize portfolio project setup

---
+++ src/Pages/Contact/Contact.tsx
@@ -0,0 +1,14 @@
+export default function Contact() {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center p-6">
+        <h1 className="text-3xl font-bold mb-4 text-center text-gray-800 dark:text-gray-100">
+          😎Welcome to Contact Page🥇
+        </h1>
+        <p className="text-gray-600 dark:text-gray-300 text-center mb-6">
+          🤷‍♂️This is a demo page.🔒 Future work will be done here.🤦‍♂️
+        </p>
+      </div>
+    </div>
+  );
+}
