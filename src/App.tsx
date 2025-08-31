1d16b8fa02ecca657c084a93efcc7a05509ce63e
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-08-31 14:27:05 +0600

    feat: Add About section with Typewriter effect, Tilt image, and CV download button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import Navbar from "./components/layout/Navbar";
+import BlurBlob from "./constants/BlurBlob";
+import About from "./Pages/About/About";
 import Projects from "./Pages/Projects/Projects";
 import Skills from "./Pages/Skills/Skills";
 
 function App() {
   return (
     <div className="container mx-auto">
+      <BlurBlob
+        position={{ top: "35%", left: "20%" }}
+        size={{ width: "30%", height: "40%" }}
+      />
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
       <Navbar />
       {/* Other components and routes would go here */}
+      <About />
       <Skills />
       <Projects />
     </div>
